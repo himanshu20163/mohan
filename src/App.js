30930b69b62ee0624c9c1d71f1da8abb184258ee
unknown <sahu.himanshu2016@gmail.com>
2023-08-31 23:37:11 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Photosearch from './Components/Photo-search/Photosearch';
-import Quiz from './Components/Quiz/Quiz'
+import Home from './Components/reactjs-urban/Home/Home';
+import Urbanrouting from './Components/reactjs-urban/Urbanrouting';
+// import Photosearch from './Components/Photo-search/Photosearch';
+// import Quiz from './Components/Quiz/Quiz'
 
 function App() {
   return (
     <div className="App" style={{width:"100%"}}>
+      {/* <Home /> */}
+      <Urbanrouting />
       {/* <Movieapp /> */}
        {/* <Photosearch /> */}
-       <Quiz />
+       {/* <Quiz /> */}
        {/* <Movieapp />  */}
        
     </div>
